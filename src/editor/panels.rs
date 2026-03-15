be122c68106cccdeaf8983f2a707c583b6d64005
Marinus <marinus.burger@gmail.com>
2026-03-15 21:35:50 +0100

    new version

--- src/editor/panels.rs
+++ src/editor/panels.rs
@@ -269,7 +269,7 @@ pub fn build_center_amp_env(params: &Arc<KickParams>, cx: &mut Context) {
             VStack::new(cx, |cx| {
                 build_preset_header(cx);
 
-                Label::new(cx, "v0.2.1")
+                Label::new(cx, "v0.2.2")
                     .class("header-version-title")
                     .height(Stretch(0.5))
                     .width(Stretch(0.2))
